1be0e883deac185622c90d70291b9167b81d80d7
AndrewStevenDJ <andrewstevendiazj@gmail.com>
2025-07-25 01:43:11 -0600

    Agrega método pasear al modelo de mascota

--- models/petModel.js
+++ models/petModel.js
@@ -6,7 +6,18 @@ const petSchema = new mongoose.Schema({
   tipo: { type: String, required: true },
   edad: { type: Number, required: true },
   usuario: { type: Number, ref: 'User', required: false },
+  felicidad: { type: Number, default: 50 },
+  hambre: { type: Number, default: 0 }
 });
+
+// MÉTODO PARA PASEAR
+petSchema.methods.pasear = function() {
+  if (typeof this.felicidad !== 'number') this.felicidad = 0;
+  if (typeof this.hambre !== 'number') this.hambre = 0;
+  this.felicidad = Math.min(this.felicidad + 10, 100);
+  this.hambre = Math.min(this.hambre + 5, 100);
+  return this.save();
+};
 
 const Pet = mongoose.model('Pet', petSchema);
 export default Pet;
